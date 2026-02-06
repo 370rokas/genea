98fced0286d2cab97b1aef54899863374c1c8035
Rokas <0x16dev@gmail.com>
2026-02-06 19:26:11 +0200

    Remove DATABASE_URL error that breaks GH Actions

--- lib/db.ts
+++ lib/db.ts
@@ -6,10 +6,6 @@ import logger from '@/lib/logger';
 
 const { Pool } = pg;
 
-if (!process.env.DATABASE_URL) {
-    throw new Error('DATABASE_URL is not defined in environment variables');
-}
-
 export const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
 });
